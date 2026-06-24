a6bc1f5c2a0fd4f67ab1229bdf66a5f58b4e6352
Matt Anis <webdev.omis@gmail.com>
2026-06-24 02:58:16 +0100

    7.8: Error boundary

--- blogger app/client/src/components/BlogList.jsx
+++ blogger app/client/src/components/BlogList.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
 const BlogList = ({ blogs }) => {
+  throw new Error('This is a test error for the ErrorBoundary component') // Test ErrorBoundary
   return (
     <div>
       <h2>blogs</h2>
--- blogger app/client/src/main.jsx
+++ blogger app/client/src/main.jsx
@@ -1,9 +1,19 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { ErrorBoundary } from 'react-error-boundary'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Router>
-    <App />
-  </Router>,
+  <ErrorBoundary
+    fallback={
+      <div>
+        <h2>Something went wrong :(</h2>
+        <span>please report bug</span>
+      </div>
+    }
+  >
+    <Router>
+      <App />
+    </Router>
+  </ErrorBoundary>,
 )
